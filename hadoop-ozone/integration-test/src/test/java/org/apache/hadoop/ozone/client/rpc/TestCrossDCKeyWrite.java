2c58437c384cdce481cde4d0bd0768fb3b205688
daniilchikk <dprizva23@gmail.com>
2025-03-21 11:45:26 +0300

    Add test timeout

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestCrossDCKeyWrite.java
@@ -64,9 +64,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+@Timeout(180)
 class TestCrossDCKeyWrite {
 
   private static MiniOzoneCluster cluster = null;
